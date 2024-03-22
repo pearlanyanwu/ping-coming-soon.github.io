9925322a3e695500cec491b3663699c067dad7be
Pearl Anyanwu <peaanyanwu@gmail.com>
2024-03-22 14:02:33 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const submit= document.getElementById("notify-button");
+const email = document.getElementById("email");
+const regex = /^[a-zA-Z0-9_]+@[a-zA-Z]+\.[a-z]{3}$/;
+
+submit.addEventListener("click", () => {
+  if (email.value == "" || !regex.test(email.value)) {
+    alert("Please provide a valid email address");
+  } else {
+    alert("Thanks for ")
+  }
+});
